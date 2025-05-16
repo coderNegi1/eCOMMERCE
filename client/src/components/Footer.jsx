0a1ad8a8b015c9da0a4e558bf3c2aa3a060f2094
coderNegi1 <prashantnegi123321@gmail.com>
2025-05-16 16:39:38 +0530

    footer logo change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
-                    <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
-                    <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight to your door. Trusted by thousend, we ain to make your shopping experience simple and affordable.</p>
+                    <img className="w-34 md:w-32" src={assets.grocery_mart_logo} alt="logo" />
+                    <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight to your door. We ain to make your shopping experience simple and affordable.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
                     {footerLinks.map((section, index) => (
