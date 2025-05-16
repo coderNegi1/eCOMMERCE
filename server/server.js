5c491f53547e8f2d6cd25d1a0f4603a7ca62564f
coderNegi1 <prashantnegi123321@gmail.com>
2025-05-16 15:36:08 +0530

    Project Completed

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary();
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://e-commerce-grocery-store-six.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
